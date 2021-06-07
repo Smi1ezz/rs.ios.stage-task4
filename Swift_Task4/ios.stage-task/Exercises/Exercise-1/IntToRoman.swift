ed8cd991caae43f1f0f5c02bacf6cdc7ed75afb3
Smi1ezz <ilya.chainikov@mail.ru>
2021-06-07 17:12:46 +0400

    ex1 done, ex2 done with inout

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,88 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        
+        guard 0 < self && self < 4000 else {
+            return nil
+        }
+        
+        var roman = [String?]()
+        
+        let selfToString = String(self)
+        
+//        guard selfToString.count <= 4 else {return nil}
+        
+        for (index, value) in selfToString.reversed().enumerated() {
+            switch index {
+            case 0:
+                switch value {
+                case "1": roman.append("I")
+                case "2": roman.append("II")
+                case "3": roman.append("III")
+                case "4": roman.append("IV")
+                case "5": roman.append("V")
+                case "6": roman.append("VI")
+                case "7": roman.append("VII")
+                case "8": roman.append("VIII")
+                case "9": roman.append("IX")
+                default:
+                    roman.append("")
+                }
+            case 1:
+                switch value {
+                case "1": roman.append("X")
+                case "2": roman.append("XX")
+                case "3": roman.append("XXX")
+                case "4": roman.append("XL")
+                case "5": roman.append("L")
+                case "6": roman.append("LX")
+                case "7": roman.append("LXX")
+                case "8": roman.append("LXXX")
+                case "9": roman.append("XC")
+                default:
+                    roman.append("")
+                }
+            case 2:
+                switch value {
+                case "1": roman.append("C")
+                case "2": roman.append("CC")
+                case "3": roman.append("CCC")
+                case "4": roman.append("CD")
+                case "5": roman.append("D")
+                case "6": roman.append("DC")
+                case "7": roman.append("DCC")
+                case "8": roman.append("DCCC")
+                case "9": roman.append("CM")
+                default:
+                    roman.append("")
+                }
+            case 3:
+                switch value {
+                case "1": roman.append("M")
+                case "2": roman.append("MM")
+                case "3": roman.append("MMM")
+                default:
+                    roman.append("")
+                }
+            default:
+                roman.append(nil)
+            }
+        }
+     
+        var result: String {
+            var k = ""
+
+            for i in roman.reversed() {
+                if let iSafe = i {
+                    k += iSafe
+                }
+                
+            }
+            return k
+        }
+        
+        guard !result.isEmpty else { return nil }
+        return result
     }
 }
+
--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -2,7 +2,56 @@ import Foundation
 
 final class FillWithColor {
     
-    func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+    func fillWithColor(_ image: inout [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
+        if row <= image.count - 1 && row >= 0 && (column <= image[row].count - 1 && column >= 0) && (image.count >= 1) && image[row].count <= 50 && (image[row][column] >= 0) && newColor < 65536 {
+            
+            let oldColour = image[row][column]
+            
+            guard oldColour != newColor else {
+                return image
+            }
+            
+            self.fillToRecursion(&image, row, column, oldColour, newColor)
+
+            return image
+        } else {
+            return image
+        }
+    }
+    
+    func fillToRecursion (_ image: inout [[Int]], _ row: Int, _ column: Int, _ oldColour: Int, _ newColor: Int) -> [[Int]] {
+        
+        if row <= image.count - 1 && row >= 0 && (column <= image[row].count - 1 && column >= 0) && (image.count >= 1) && image[row].count <= 50 && (image[row][column] >= 0) && newColor < 65536 && image[row][column] == oldColour {
+            
+            image[row][column] = newColor
+            self.fillToRecursion(&image, row+1, column, oldColour, newColor)
+            self.fillToRecursion(&image, row-1, column, oldColour, newColor)
+            self.fillToRecursion(&image, row, column+1, oldColour, newColor)
+            self.fillToRecursion(&image, row, column-1, oldColour, newColor)
+            return image
+            
+        } else {
+            return image
+        }
     }
 }
+/*
+ 
+ **Input: image = [[1, 1, 1],
+                   [1, 1, 0],
+                   [1, 0, 1]], row = 1, column = 1, newColor = 2**
+ **Output: [[2, 2, 2],
+            [2, 2, 0],
+            [2, 0, 1]]**
+ 
+## Constraints
+
+**`m == image.length`**
+**`n == image[i].length`**
+**`1 <= m, n <= 50`**
+**`0 <= image[i][j], newColor < 65536`**
+**`0 <= row <= m`**
+**`0 <= column <= n`**
+
+*/
+
